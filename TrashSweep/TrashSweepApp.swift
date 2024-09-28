63a179d90bccf4849b6bf534a5434cfa32e7cd74
congtranit <congtranit@gmail.com>
2024-09-28 17:13:28 +0700

    Initial Commit

---
+++ TrashSweep/TrashSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrashSweepApp.swift
+//  TrashSweep
+//
+//  Created by Tran Cong on 28/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TrashSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrashSweepTests/TrashSweepTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrashSweepTests.swift
+//  TrashSweepTests
+//
+//  Created by Tran Cong on 28/9/24.
+//
+
+import Testing
+@testable import TrashSweep
+
+struct TrashSweepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
